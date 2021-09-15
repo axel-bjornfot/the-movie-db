9bfccada4b769e9f60c49069f19844af6331e141
Axel Björnfot <axelbjornfot@gmail.com>
2021-09-15 13:49:12 +0200

    styling person page

--- src/pages/PersonPage.jsx
+++ src/pages/PersonPage.jsx
@@ -8,7 +8,6 @@ import Image from "react-bootstrap/Image";
 import { useQueries, useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { getPerson, getPersonCredits } from "../services/TMDB";
-import PersonCredits from "../components/PersonCredits";
 
 const PersonPage = () => {
 	const { id } = useParams();
@@ -37,24 +36,56 @@ const PersonPage = () => {
 
 			{data?.results && (
 				<>
-					{data.results.profile_path && (
+					<Col xs={7} md={5} lg={4} xl={4} className="pt-4 m-auto">
 						<Card text="light" bg="info">
-							<Card.Img
-								src={`https://image.tmdb.org/t/p/w500/${data.results.profile_path}`}
-								alt="Card image"
-							/>
+							{data.results.profile_path && (
+								<Card.Img
+									src={`https://image.tmdb.org/t/p/w500/${data.results.profile_path}`}
+									alt="Card image"
+								/>
+							)}
 							<Card.ImgOverlay></Card.ImgOverlay>
 						</Card>
-					)}
-
-					<Card style={{ width: "20em" }}>
-						<Card.Body>
-							<Card.Title>{data.results.name}</Card.Title>
-							<Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
-							<Card.Text>{data.results.biography}</Card.Text>
-						</Card.Body>
-					</Card>
-					<PersonCredits></PersonCredits>
+						<Card>
+							<Card.Body>
+								<Card.Title>{data.results.name}</Card.Title>
+								<Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+								<Card.Text>{data.results.biography}</Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+					<h1 className="text-light pt-5">Har också medvaarkat i</h1>
+					<Row className="pt-4">
+						{data.results.credits.cast.map((movie, i) => (
+							<Col
+								key={i}
+								xs={5}
+								md={4}
+								lg={3}
+								xl={2}
+								className="pt-4 m-auto"
+							>
+								<Card text="light" bg="info">
+									<Card.Body>
+										{(movie.poster_path && (
+											<Card.Img
+												src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+												alt="Card image"
+											/>
+										)) || (
+											<Card.Img
+												src={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimgix.ranker.com%2Fuser_node_img%2F66%2F1304212%2Foriginal%2Fjohn-cena-photo-u61%3Ffm%3Dpjpg%26q%3D80&f=1&nofb=1`}
+												alt="Card image"
+											/>
+										)}
+										<Card.Title key={i}>
+											{movie.title}
+										</Card.Title>
+									</Card.Body>
+								</Card>
+							</Col>
+						))}
+					</Row>
 				</>
 			)}
 		</Container>
